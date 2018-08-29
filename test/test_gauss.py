afa2372cd4fcd0ecb5340b3cbc2418acee55e10f
David W Wright <dave.william.wright@gmail.com>
2018-08-29 14:58:53 +0100

    Change styling of module names

--- test/test_gauss.py
+++ test/test_gauss.py
@@ -2,10 +2,10 @@ import os, sys
 import easyvvuq as uq
 
 campaign = uq.Campaign(state_fname="test_input/test_gauss.json")
-uq.UQP.basicUQP(campaign)
+uq.uqp.basicUQP(campaign)
 uq.populate_runs_dir(campaign)
 uq.apply_for_each_run(campaign, uq.execute_local)
-uq.apply_for_each_run(campaign, uq.UQP.statsUQP(reader=uq.Reader.csvReader('output.csv', 1)))
+uq.apply_for_each_run(campaign, uq.uqp.statsUQP(reader=uq.reader.csvReader('output.csv', 1)))
 campaign.print()
 campaign.save_state("out_gauss.json")
 
