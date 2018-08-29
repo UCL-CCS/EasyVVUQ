79441febaab0e53ce5bb32657c0dafc9f586b60a
David W Wright <dave.william.wright@gmail.com>
2018-08-29 14:45:53 +0100

    Correct loading of modules

--- easyvvuq/__init__.py
+++ easyvvuq/__init__.py
@@ -1,7 +1,6 @@
-from EasyVVUQ.Campaign import Campaign
-from EasyVVUQ.populate_runs_dir import populate_runs_dir
-from EasyVVUQ.apply_for_each_run import apply_for_each_run
-from EasyVVUQ.Execute import execute_local
-import EasyVVUQ.UQP
-import EasyVVUQ.Reader
-
+from .Campaign import Campaign
+from .populate_runs_dir import populate_runs_dir
+from .apply_for_each_run import apply_for_each_run
+from .Execute import execute_local
+from . import UQP
+from . import Reader
