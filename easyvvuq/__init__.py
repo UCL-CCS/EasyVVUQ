1f5e886a3045d1a02c2dbf50884e1fbf2a086942
Robin Richardson <raarichardson@gmail.com>
2018-09-05 16:38:38 +0100

    Merge apply_for_each_run.py and populate_runs_dir.py into campaign (adding them as methods)

--- easyvvuq/__init__.py
+++ easyvvuq/__init__.py
@@ -1,7 +1,5 @@
 import json
 from .campaign import Campaign
-from .populate_runs_dir import populate_runs_dir
-from .apply_for_each_run import apply_for_each_run
 from .execute import execute_local
 from . import uqp
 from . import reader
--- test/test_gauss.py
+++ test/test_gauss.py
@@ -3,9 +3,9 @@ import easyvvuq as uq
 
 my_campaign = uq.Campaign(state_filename="test_input/test_gauss.json")
 uq.uqp.basicUQP(my_campaign)
-uq.populate_runs_dir(my_campaign)
-uq.apply_for_each_run(my_campaign, uq.execute_local)
-uq.apply_for_each_run(my_campaign, uq.uqp.statsUQP(reader=uq.reader.csvReader('output.csv', 1)))
+my_campaign.populate_runs_dir()
+my_campaign.apply_for_each_run(uq.execute_local)
+my_campaign.apply_for_each_run(uq.uqp.statsUQP(reader=uq.reader.csvReader('output.csv', 0)))
 my_campaign.print()
 my_campaign.save_state("out_gauss.json")
 
