633f6ff5ad0d5727b11062d4bb5958f9148aa344
David W Wright <dave.william.wright@gmail.com>
2018-09-06 12:53:39 +0100

    Add LGPL test boilerplate to files

--- easyvvuq/uqp/statsUQP.py
+++ easyvvuq/uqp/statsUQP.py
@@ -2,6 +2,29 @@ import os, sys
 import numpy as np
 import csv
 
+__copyright__ = """
+
+    Copyright 2018 Robin A. Richardson, David W. Wright 
+
+    This file is part of EasyVVUQ 
+
+    EasyVVUQ is free software: you can redistribute it and/or modify
+    it under the terms of the Lesser GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    EasyVVUQ is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    Lesser GNU General Public License for more details.
+
+    You should have received a copy of the Lesser GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+"""
+__license__ = "LGPL"
+
+
 def statsUQP(reader=None):
     if reader == None:
         sys.exit("A reader (e.g. csvReader) must be specified for the appropriate file type")
