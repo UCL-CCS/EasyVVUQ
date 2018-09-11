0bedc19fab6f1386d2da4be5456cae6fdd096e66
Robin Richardson <robin.richardson@ucl.ac.uk>
2018-09-11 17:54:07 +0100

    Clean up test script

--- test/test_cannonsim.py
+++ test/test_cannonsim.py
@@ -24,17 +24,22 @@ __copyright__ = """
 __license__ = "LGPL"
 
 
+# Get app and param info about cannonsim
 my_campaign = uq.Campaign(state_filename="test_input/test_cannonsim.json")
 
-my_campaign.vary_param("angle",    dist=uq.distributions.uniform(0.0,  1.0))
+# Specify names of parameters that are variable, and their (prior) distributions.
+my_campaign.vary_param("angle",    dist=uq.distributions.uniform(0.0, 1.0))
 my_campaign.vary_param("velocity", dist=uq.distributions.normal(10.0, 1.0))
 my_campaign.vary_param("mass",     dist=uq.distributions.customHistogram("test_input/mass_distribution.csv"))
 
+# Apply the randomSampler UQP, generating 15 runs
 uq.uqp.sampling.randomSampler(my_campaign, num_samples=15)
 
+# Execute and analyse
 my_campaign.populate_runs_dir()
 my_campaign.apply_for_each_run(uq.execute_local)
 my_campaign.apply_for_each_run(uq.uqp.analysis.basicStats(reader=uq.reader.csvReader('output.csv', 0)))
 
+# Output
 print(my_campaign)
 my_campaign.save_state("out_cannonsim.json")
