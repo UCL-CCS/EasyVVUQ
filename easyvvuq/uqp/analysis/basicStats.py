748d9f579df377ceb47c9ad637fac5c20d3d8a09
David W Wright <dave.william.wright@gmail.com>
2018-10-01 16:55:08 +0100

    Start of update to sensible form of the basis stats uqp

--- easyvvuq/uqp/analysis/basicStats.py
+++ easyvvuq/uqp/analysis/basicStats.py
@@ -1,6 +1,7 @@
-import os, sys
-import numpy as np
-import csv
+import os
+import pandas as pd
+from easyvvuq import OutputType
+from .base import BaseAnalysisUQP
 
 __copyright__ = """
 
@@ -25,16 +26,76 @@ __copyright__ = """
 __license__ = "LGPL"
 
 
-def basicStats(reader=None):
-    if reader is None:
-        sys.exit("A reader (e.g. csvReader) must be specified for the appropriate file type")
+class BasicStats(BaseAnalysisUQP):
 
-    def basicStats_specific(dirname):
-        l = reader(dirname)
-        mean = np.mean(l)
-        std = np.std(l)
+    def __init__(self, data_src, params_cols=[], value_cols=[],
+                 *args, **kwargs):
 
-        result = {"UQP": "basicStats", "mean": mean, "std": std}
-        return result
+        self.uqp_name = 'basic_stats'
 
-    return basicStats_specific
+        # Handles creation of `self.data_src` attribute (dict)
+        super().__init__(data_src, uqp_name=self.uqp_name, *args, **kwargs)
+
+        data_src = self.data_src
+
+        if data_src:
+
+            if 'files' in data_src:
+
+                if len(data_src['files']) != 1:
+
+                    raise RuntimeError("Data source must contain a SINGLE file path for this UQP")
+
+                else:
+
+                    self.data_frame = pd.read_csv(data_src['files'][0], sep='\t')
+
+        self.value_cols = value_cols
+
+        if self.campaign is not None:
+
+            if not params_cols:
+                self.params_cols = list(self.campaign.params_info.keys())
+
+            self.value_cols = self.campaign.decoder.output_columns
+
+        else:
+
+            self.params_cols = params_cols
+
+        self.output_type = OutputType.SUMMARY
+
+    def run_analysis(self):
+
+        if self.data_frame is None:
+            raise RuntimeError("UQP needs a data frame to analyse")
+
+        df = self.data_frame
+
+        output_dir = self.output_dir
+        output_file = os.path.join(output_dir, 'ensemble_boot.tsv')
+
+        grouped_data = df.groupby(self.params_cols)
+
+        # Apply bootstrapping to all value columns selected
+        # Note results come a tuple per cell
+        results = grouped_data.describe()
+        results.to_csv(output_file, sep='\t')
+
+        log_name = f"{self.uqp_name}.json"
+        self.log_vars(log_name)
+
+        if self.campaign is not None:
+
+            state_file = os.path.join(output_dir, 'state_file.json')
+            self.campaign.save_state(state_file)
+
+            if stat_func is not None:
+                self.campaign.record_analysis('ensemble_bootstrap',
+                                              output_file
+                                              )
+            else:
+                self.campaign.record_analysis('ensemble_bootstrap',
+                                              output_file
+                                              )
+        return results, output_file
