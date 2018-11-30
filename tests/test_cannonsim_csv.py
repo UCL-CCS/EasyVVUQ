bbf0e24b0ec159f306e14096faee4482cfcf35e1
Robin Richardson <robin.richardson@ucl.ac.uk>
2018-11-30 20:24:41 +0000

    Remove whitespace from test_cannonsim

--- tests/test_cannonsim_csv.py
+++ tests/test_cannonsim_csv.py
@@ -51,7 +51,7 @@ def test_cannonsim_csv(tmpdir):
     assert("gravity" in my_campaign.params_info)
     assert("mass" in my_campaign.params_info)
     assert("velocity" in my_campaign.params_info)
- 
+
     my_campaign.vary_param("angle",    dist=uq.distributions.uniform(0.0, 1.0))
     my_campaign.vary_param("height",   dist=uq.distributions.uniform_integer(0, 10))
     my_campaign.vary_param("velocity", dist=uq.distributions.normal(10.0, 1.0))
@@ -59,7 +59,7 @@ def test_cannonsim_csv(tmpdir):
         "mass",
         dist=uq.distributions.custom_histogram("tests/cannonsim/test_input/mass_distribution.csv")
     )
-    
+ 
     assert("angle" in my_campaign.vars)
     assert("height" in my_campaign.vars)
     assert("velocity" in my_campaign.vars)
