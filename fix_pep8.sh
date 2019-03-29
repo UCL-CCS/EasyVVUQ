780764d600dfe16dd919b809499e38af377d54ad
Robin Richardson <robin.richardson@ucl.ac.uk>
2019-03-29 14:36:27 +0000

    Add fix_pep8.sh script to run appropriate autopep8 on easyvvuq/ and tests/ dirs without breaking anything...

---
+++ fix_pep8.sh
@@ -0,0 +1,2 @@
+autopep8 --in-place --aggressive --aggressive --aggressive --exclude easyvvuq/__init__.py -r easyvvuq/
+autopep8 --in-place --aggressive --aggressive --aggressive --exclude easyvvuq/__init__.py -r tests/
