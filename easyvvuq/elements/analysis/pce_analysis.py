fb0c7aa0e3671907b2c6b90765e9bba3aaad5690
David W Wright <dave.william.wright@gmail.com>
2019-05-08 15:20:08 +0100

    Initial attempt to update the PCE analysis step.

--- easyvvuq/elements/analysis/pce_analysis.py
+++ easyvvuq/elements/analysis/pce_analysis.py
@@ -1,6 +1,3 @@
-import os
-import numpy as np
-import pandas as pd
 import chaospy as cp
 from easyvvuq import OutputType
 from .base import BaseAnalysisElement
@@ -22,82 +19,84 @@ class PCEAnalysis(BaseAnalysisElement):
     def element_version(self):
         return "0.3"
 
-    def __init__(self, params_cols=None):
+    def __init__(self, params_cols=None, sampler=None):
 
-        # TODO: Fix this to allow more flexibility - basically pass through
-        # available options to `pd.DataFrame.describe()`
-
-        # Handles creation of `self.data_src` attribute (dict)
-        super().__init__()
+        if sampler is None:
+            msg = 'PCE analysis requires a paired sampler to be passed'
+            raise RuntimeError(msg)
 
         self.params_cols = params_cols
         self.output_type = OutputType.SUMMARY
+        self.sampler = sampler
 
-        # TODO fix call __dict___ in log_analysis to allow ndarray
-        # cf. casting ndarray to list in _apply_analysis
-        # To store Descriptive Statistics
         self._statistical_moments = {}
         self._percentiles = {}
         self._sobol_indices = {}
-        # self._correlation_matrices = {}
-        # self._output_distributions = {}
+        self._correlation_matrices = {}
+        self._output_distributions = {}
 
-    def analyse(self, data_frame=None):
+    def analyse(self, data_frame=None, qoi_cols=None):
 
         if data_frame is None:
             raise RuntimeError("Analysis element needs a data frame to "
                                "analyse")
 
+        if qoi is None:
+            raise RuntimeError("Analysis element requires a list of "
+                               "quantities of interest (qoi)")
+
         # TODO: refactor the rest of this
 
         # Get the Polynomial
-        P = self.campaign.P
+        P = self.sampler.P
 
         # Compute nodes and weights
-        nodes, weights = cp.generate_quadrature(order=self.campaign.quad_order,
-                                                domain=self.campaign.distribution,
-                                                rule=self.campaign.quad_rule,
-                                                sparse=self.campaign.quad_sparse)
+        nodes, weights = cp.generate_quadrature(order=self.sampler.quad_order,
+                                                domain=self.sampler.distribution,
+                                                rule=self.sampler.quad_rule,
+                                                sparse=self.sampler.quad_sparse)
 
         # Extract output values for each quantity of interest from Dataframe
-        samples = {k: [] for k in self.value_cols}
+        samples = {k: [] for k in qoi_cols}
         for i in range(self.campaign.run_number):
-            for k in self.value_cols:
+            for k in qoi_cols:
                 values = data_frame.loc[data_frame['run_id'] == 'Run_' + str(i)][k].to_numpy()
                 samples[k].append(values)
 
         output_distributions = {}
+
         # Compute descriptive statistics for each quantity of interest
-        for k in self.value_cols:
+        for k in qoi_cols:
             # Approximation solver
             fit = cp.fit_quadrature(P, nodes, weights, samples[k])
 
             # Statistical moments
-            mean = cp.E(fit, self.campaign.distribution)
-            var = cp.Var(fit, self.campaign.distribution)
-            std = cp.Std(fit, self.campaign.distribution)
-            self._statistical_moments[k] = {'mean': list(mean), 'var':
-                                            list(var), 'std': list(std)}
+            mean = cp.E(fit, self.sampler.distribution)
+            var = cp.Var(fit, self.sampler.distribution)
+            std = cp.Std(fit, self.sampler.distribution)
+            self._statistical_moments[k] = {'mean': mean,
+                                            'var': var,
+                                            'std': std}
 
             # Percentiles (Pxx)
-            P10 = cp.Perc(fit, 10, self.campaign.distribution)
-            P90 = cp.Perc(fit, 90, self.campaign.distribution)
-            self._percentiles[k] = {'p10': list(P10), 'p90': list(P90)}
+            P10 = cp.Perc(fit, 10, self.sampler.distribution)
+            P90 = cp.Perc(fit, 90, self.sampler.distribution)
+            self._percentiles[k] = {'p10': P10, 'p90': P90}
 
             # First Sobol indices
-            sobol_first_narr = cp.Sens_m(fit, self.campaign.distribution)
+            sobol_first_narr = cp.Sens_m(fit, self.sampler.distribution)
             sobol_first_dict = {}
             i_par = 0
-            for param_name in self.campaign.vars.keys():
-                sobol_first_dict[param_name] = list(sobol_first_narr[i_par])
+            for param_name in self.sampler.vary.keys():
+                sobol_first_dict[param_name] = sobol_first_narr[i_par]
                 i_par += 1
             self._sobol_indices[k] = sobol_first_dict
 
             # Correlation matrix
-            #self._correlation_matrices[k] = list(cp.Corr(fit, self.campaign.distribution))
+            self._correlation_matrices[k] = cp.Corr(fit, self.sampler.distribution)
 
             # Ouput distributions
-            #output_distributions[k] = cp.QoI_Dist(fit, self.campaign.distribution)
+            output_distributions[k] = cp.QoI_Dist(fit, self.sampler.distribution)
 
         return
 
@@ -110,7 +109,7 @@ class PCEAnalysis(BaseAnalysisElement):
         values: ndarrays
         """
 
-        if qoi not in self.value_cols:
+        if qoi not in self._statistical_moments:
             raise NameError("Unknown quantity of interset " + qoi)
 
         return self._statistical_moments[qoi]
@@ -123,44 +122,44 @@ class PCEAnalysis(BaseAnalysisElement):
         values: ndarrays
         """
 
-        if qoi not in self.value_cols:
-            raise NameError("Unknown quantity of interset " + qoi)
+        if qoi not in self._percentiles:
+            raise NameError("Unknown quantity of interest " + qoi)
 
         return self._percentiles[qoi]
 
     def sobol_indices(self, qoi, typ):
         """
-        Returns a dictionary object containing the sobol indices of the given quantity of interset
+        Returns a dictionary object containing the sobol indices of the given quantity of interest
         for each uncertain parameter (typ='first_order').
         keys: uncertain parameters names
         values: ndarray
         """
 
-        if qoi not in self.value_cols:
-            raise NameError("Unknown quantity of interset " + qoi)
+        if qoi not in self._sobol_indices:
+            raise NameError("Unknown quantity of interest " + qoi)
 
         if typ == 'first_order':
             return self._sobol_indices[qoi]
         else:
-            print('Not yet implemented.')
-            pass
+            raise RuntimeError('Calculation of higher order '
+                               'indices not implemented')
 
-#    def correlation_matrix(self, qoi):
-#        """
-#        Returns the correlation matrix of the given quantity of interest.
-#        """
-#
-#        if qoi not in self.value_cols:
-#            raise NameError("Unknown quantity of interset " + qoi)
-#
-#        return self._correlation_matrices[qoi]
-#
-#    def output_distributions(self, qoi):
-#        """
-#        Returns the constructed quantity of interest distributions.
-#        """
-#
-#        if qoi not in self.value_cols:
-#            raise NameError("Unknown quantity of interset " + qoi)
-#
-#        return self._output_distributions[qoi_name]
+    def correlation_matrix(self, qoi):
+        """
+        Returns the correlation matrix of the given quantity of interest.
+        """
+
+        if qoi not in self._correlation_matrices:
+            raise NameError("Unknown quantity of interest " + qoi)
+
+        return self._correlation_matrices[qoi]
+
+    def output_distributions(self, qoi):
+        """
+        Returns the constructed quantity of interest distributions.
+        """
+
+        if qoi not in self._output_distributions:
+            raise NameError("Unknown quantity of interest " + qoi)
+
+        return self._output_distributions[qoi]
