0acc3167762374a6864adc0c888555fd76aaf1fd
David W Wright <dave.william.wright@gmail.com>
2019-05-30 13:14:03 +0100

    pypi friendly description added

--- setup.py
+++ setup.py
@@ -1,4 +1,10 @@
+from os import path
 from setuptools import setup, find_packages
+
+# read the contents of README file
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='easyvvuq',
@@ -8,7 +14,8 @@ setup(
     description=('Library to facilitate simple Verification, Validation and '
                  'Uncertainty Quantification of simulation codes'),
 
-    long_description='Copy from README file',
+    long_description=long_description,
+    long_description_content_type='text/markdown'
 
     url='http://ccs.chem.ucl.ac.uk',
 
