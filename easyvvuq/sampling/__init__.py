a097225d3fc2042ac20f42c9cf4d3b065c472c71
Robin Richardson <robin.richardson@ucl.ac.uk>
2019-06-18 14:30:27 +0100

    Add SweepSampler to easyvvuq samplers

--- easyvvuq/sampling/__init__.py
+++ easyvvuq/sampling/__init__.py
@@ -2,6 +2,7 @@ from .base import BaseSamplingElement, Vary
 from .random import RandomSampler
 from .stochastic_collocation import SCSampler
 from .pce import PCESampler
+from .sweep import SweepSampler
 
 __copyright__ = """
 
