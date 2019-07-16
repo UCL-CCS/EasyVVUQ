dd72c8079eda1696ac94f07ae5e84cfd4cdba42e
di73kuj2 <vytautas.jancauskas@lrz.de>
2019-07-16 15:24:12 +0200

    Fix the sqlite issue with too many variables in the query

--- easyvvuq/db/sql.py
+++ easyvvuq/db/sql.py
@@ -470,14 +470,13 @@ class CampaignDB(BaseCampaignDB):
         -------
 
         """
-
-        selected = self.session.query(RunTable).filter(
-            RunTable.run_name.in_(set(run_ID_list))).all()
-
-        for run in selected:
-            run.status = status
-
-        self.session.commit()
+        max_entries = 900
+        for i in range(0, len(run_ID_list), max_entries):
+            selected = self.session.query(RunTable).filter(
+                RunTable.run_name.in_(set(run_ID_list[i:i + max_entries]))).all()
+            for run in selected:
+                run.status = status
+            self.session.commit()
 
     def campaigns(self):
         """Get list of campaigns for which information is stored in the
