bfda655b9aefeb747ff5d2a56809ba6e68000061
orbitfold <vytautas.jancauskas@lrz.de>
2019-07-29 11:40:23 +0200

    Added SALib as a dependency

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     install_requires=['numpy', 'pandas>=0.24', 'scipy',
                       'pytest', 'SQLAlchemy', 'chaospy',
                       'sqlalchemy-utils', 'matplotlib',
-                      'jsonpickle', 'cerberus'],
+                      'jsonpickle', 'cerberus', 'SALib'],
 
     packages=find_packages(),
 
