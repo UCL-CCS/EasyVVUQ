05eb2a77555e49648bedf40462f1a536bb85f516
orbitfold <vytautas.jancauskas@lrz.de>
2019-08-14 15:35:15 +0200

    Added a campaign fixture:

--- tests/test_db.py
+++ tests/test_db.py
@@ -5,11 +5,19 @@ from easyvvuq.constants import default_campaign_prefix
 from easyvvuq.db.sql import CampaignDB
 from easyvvuq.data_structs import CampaignInfo
 
-def test_file_created(tmp_path):
+@pytest.fixture
+def campaign(tmp_path):
     info = CampaignInfo(
         name='test',
         campaign_dir_prefix=default_campaign_prefix,
         easyvvuq_version=easyvvuq.__version__,
         campaign_dir='.')
     campaign = CampaignDB(location='sqlite:///{}/test.sqlite'.format(tmp_path), new_campaign=True, name='test', info=info)
-    assert(os.path.isfile('{}/test.sqlite'.format(tmp_path)))
+    campaign.tmp_path = tmp_path
+    return campaign
+
+def test_db_file_created(campaign):
+    assert(os.path.isfile('{}/test.sqlite'.format(campaign.tmp_path)))
+
+def test_add_runs(tmp_path):
+    pass
