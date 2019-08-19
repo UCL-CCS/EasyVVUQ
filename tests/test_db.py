b029d568a6fbacfc0e8ddb2439cd625012a132c5
orbitfold <vytautas.jancauskas@lrz.de>
2019-08-19 16:01:07 +0200

    Added test_add_runs

--- tests/test_db.py
+++ tests/test_db.py
@@ -3,7 +3,8 @@ import os.path
 import easyvvuq
 from easyvvuq.constants import default_campaign_prefix
 from easyvvuq.db.sql import CampaignDB
-from easyvvuq.data_structs import CampaignInfo
+from easyvvuq.data_structs import CampaignInfo, RunInfo
+
 
 @pytest.fixture
 def campaign(tmp_path):
@@ -16,8 +17,14 @@ def campaign(tmp_path):
     campaign.tmp_path = tmp_path
     return campaign
 
+
 def test_db_file_created(campaign):
     assert(os.path.isfile('{}/test.sqlite'.format(campaign.tmp_path)))
 
-def test_add_runs(tmp_path):
-    pass
+
+def test_add_runs(campaign):
+    run1 = RunInfo('run1', 'test', '.', 1, {'a' : 1}, 1, 1)
+    run2 = RunInfo('run2', 'test', '.', 1, {'a' : 1}, 1, 1)
+    campaign.add_runs([run1, run2])
+    campaign.get_run_status('run1', 'test', 1)
+    campaign.get_run_status('run2', 'test', 1)
