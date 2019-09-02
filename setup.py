50d581ba5701774beebea3b2109fd65f89335a90
David W Wright <dave.william.wright@gmail.com>
2019-09-02 15:50:21 +0100

    Fix whitespace

--- setup.py
+++ setup.py
@@ -2,8 +2,6 @@ from os import path
 from setuptools import setup, find_packages
 import versioneer
 
-
-
 # read the contents of README file
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
