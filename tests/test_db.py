45d3d31de7c9f14dbf145fe7d0500eb3a6f8ed99
di73kuj2 <vytautas.jancauskas@lrz.de>
2019-09-04 15:19:40 +0200

    pep8ing it up

--- tests/test_db.py
+++ tests/test_db.py
@@ -10,33 +10,33 @@ from easyvvuq.constants import Status
 @pytest.fixture
 def app_info():
     app_info = AppInfo('test', uq.ParamsSpecification({
-    "temp_init": {
-        "type": "float",
-        "min": 0.0,
-        "max": 100.0,
-        "default": 95.0},
-    "kappa": {
-        "type": "float",
-        "min": 0.0,
-        "max": 0.1,
-        "default": 0.025},
-    "t_env": {
-        "type": "float",
-        "min": 0.0,
-        "max": 40.0,
-        "default": 15.0},
-    "out_file": {
-        "type": "string",
-        "default": "output.csv"}
-    }),
-    None,
-    uq.encoders.GenericEncoder(
-        template_fname='tests/cooling/cooling.template',
-        delimiter='$',
-        target_filename='cooling_in.json'),
-    uq.decoders.SimpleCSV(target_filename='output.csv',
-                              output_columns=["te", "ti"],
-                              header=0))
+        "temp_init": {
+            "type": "float",
+            "min": 0.0,
+            "max": 100.0,
+            "default": 95.0},
+        "kappa": {
+            "type": "float",
+            "min": 0.0,
+            "max": 0.1,
+            "default": 0.025},
+        "t_env": {
+            "type": "float",
+            "min": 0.0,
+            "max": 40.0,
+            "default": 15.0},
+        "out_file": {
+            "type": "string",
+            "default": "output.csv"}}),
+        None,
+        uq.encoders.GenericEncoder(
+            template_fname='tests/cooling/cooling.template',
+            delimiter='$',
+            target_filename='cooling_in.json'),
+        uq.decoders.SimpleCSV(
+            target_filename='output.csv',
+            output_columns=["te", "ti"],
+            header=0))
     return app_info
 
 
@@ -47,9 +47,10 @@ def campaign(tmp_path, app_info):
         campaign_dir_prefix=default_campaign_prefix,
         easyvvuq_version=uq.__version__,
         campaign_dir=str(tmp_path))
-    campaign = CampaignDB(location='sqlite:///{}/test.sqlite'.format(tmp_path), new_campaign=True, name='test', info=info)
+    campaign = CampaignDB(location='sqlite:///{}/test.sqlite'.format(tmp_path),
+                          new_campaign=True, name='test', info=info)
     campaign.tmp_path = str(tmp_path)
-    runs = [RunInfo('run', 'test', '.', 1, {'a' : 1}, 1, 1) for _ in range(1010)]
+    runs = [RunInfo('run', 'test', '.', 1, {'a': 1}, 1, 1) for _ in range(1010)]
     run_names = ['Run_{}'.format(i) for i in range(1, 1011)]
     campaign.add_runs(runs)
     campaign.add_app(app_info)
@@ -78,7 +79,7 @@ def test_app(campaign):
     assert(app_dict['name'] == 'test')
     assert(isinstance(app_dict, dict))
 
-    
+
 def test_add_app(campaign, app_info):
     with pytest.raises(RuntimeError):
         campaign.add_app(app_info)
@@ -93,7 +94,6 @@ def test_get_campaign_id(campaign):
         campaign.get_campaign_id('test_')
     assert(campaign.get_campaign_id('test') == 1)
 
-    
+
 def test_campaign_dir(campaign):
     assert(campaign.campaign_dir('test') == campaign.tmp_path)
-
