64e28ee0971b3bd22146ebc031b570aab7c9745d
Robin Richardson <robin.richardson@ucl.ac.uk>
2019-09-18 13:50:49 +0100

    Updat multisampler test

--- tests/test_multisampler.py
+++ tests/test_multisampler.py
@@ -82,7 +82,7 @@ def test_worker(tmpdir):
             "max": 1000.0,
             "default": 10.0}}
 
-    # Create an encoder and decoder for the cannonsim app
+    # Create an encoder, decoder and collater for the cannonsim app
     encoder = uq.encoders.GenericEncoder(
         template_fname='tests/cannonsim/test_input/cannonsim.template',
         delimiter='#',
@@ -90,20 +90,19 @@ def test_worker(tmpdir):
     decoder = uq.decoders.SimpleCSV(
         target_filename='output.csv', output_columns=[
             'Dist', 'lastvx', 'lastvy'], header=0)
+    collater = uq.collate.AggregateSamples(average=False)
 
     # Add the cannonsim app
     my_campaign.add_app(name="cannonsim",
                         params=params,
                         encoder=encoder,
-                        decoder=decoder)
+                        decoder=decoder,
+                        collater=collater)
 
     # Set the active app to be cannonsim (this is redundant when only one app
     # has been added)
     my_campaign.set_app("cannonsim")
 
-    # Create a collation element for this campaign
-    collater = uq.collate.AggregateSamples(average=False)
-    my_campaign.set_collater(collater)
 
     # Set up samplers
     sweep1 = {
