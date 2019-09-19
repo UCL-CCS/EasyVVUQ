2563c3e50cb72e536f15943d527aad1cfbc9b16c
Robin Richardson <robin.richardson@ucl.ac.uk>
2019-09-19 17:33:24 +0100

    Tidy up spacing

--- tests/test_multiapp.py
+++ tests/test_multiapp.py
@@ -145,8 +145,8 @@ def test_multiapp(tmpdir):
     my_campaign = uq.Campaign(name='multiapp', work_dir=tmpdir)
 
     # Add the cannonsim app to the campaign
-    (params, encoder, decoder, collater,
-    cannon_sampler, cannon_action, cannon_stats) = setup_cannonsim_app()
+    (params, encoder, decoder, collater, cannon_sampler,
+     cannon_action, cannon_stats) = setup_cannonsim_app()
     my_campaign.add_app(name="cannonsim",
                         params=params,
                         encoder=encoder,
@@ -158,7 +158,7 @@ def test_multiapp(tmpdir):
 
     # Add the cooling app to the campaign
     (params, encoder, decoder, collater, cooling_sampler,
-    cooling_action, cooling_stats) = setup_cooling_app()
+     cooling_action, cooling_stats) = setup_cooling_app()
     my_campaign.add_app(name="cooling",
                         params=params,
                         encoder=encoder,
