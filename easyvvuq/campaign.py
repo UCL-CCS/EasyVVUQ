2bbfce38c76565c3b4934d3b09cc334c7165ab34
di73kuj2 <vytautas.jancauskas@lrz.de>
2019-09-27 14:05:55 +0200

    Added campaign_dask.py

--- easyvvuq/campaign.py
+++ easyvvuq/campaign.py
@@ -581,6 +581,10 @@ class Campaign:
         create input files for it in each run directory, usually with varying
         input (scientific) parameters.
 
+        Parameters
+        ----------
+        client - dask jobqueue client
+
         Returns
         -------
 
@@ -648,16 +652,12 @@ class Campaign:
         Returns
         -------
         """
-
+        
         # Loop through all runs in this campaign with status ENCODED, and
         # run the specified action on each run's dir
-        #for run_id, run_data in self.campaign_db.runs(status=status, app_id=self._active_app['id']):
-        #    logger.info("Applying " + action.__module__ + " to " + run_data['run_dir'])
-        #    action.act_on_dir(run_data['run_dir'])
-        run_dirs = [run_data['run_dir'] for run_id, run_data in
-                    self.campaign_db.runs(status=status, app_id=self._active_app['id'])]
-        b = db.from_sequence(run_dirs)
-        b.map(action.act_on_dir).compute()
+        for run_id, run_data in self.campaign_db.runs(status=status, app_id=self._active_app['id']):
+           logger.info("Applying " + action.__module__ + " to " + run_data['run_dir'])
+           action.act_on_dir(run_data['run_dir'])
 
     def collate(self):
         """Combine the output from all runs associated with the current app.
---
+++ easyvvuq/campaign_dask.py
@@ -0,0 +1,8 @@
+from easyvvuq import Campaign
+
+class CampaignDask(Campaign):
+    def populate_runs_dir(self, client):
+        pass
+
+    def apply_for_each_run_dir(self, client):
+        pass
