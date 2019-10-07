1248f6a474a6e189b7d71d05e57705bc1c5b0922
Robin Richardson <robin.richardson@ucl.ac.uk>
2019-10-07 16:52:57 +0100

    Add new ApplyFixtures encoder, containing all the fixtures logic that is currently spread around the whole library

--- easyvvuq/encoders/__init__.py
+++ easyvvuq/encoders/__init__.py
@@ -2,6 +2,7 @@ from .base import BaseEncoder
 from .generic_template import GenericEncoder
 from .multiencoder import MultiEncoder
 from .directory_builder import DirectoryBuilder
+from .fixtures import ApplyFixtures
 
 __copyright__ = """
 
