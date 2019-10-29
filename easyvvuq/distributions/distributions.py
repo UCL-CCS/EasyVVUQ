1e54280df235f01bc9483611ce36876e24ee250a
Jalal Lakhlili <jlakhlili@gmail.com>
2019-10-29 15:22:57 +0100

    Use DiscreteUniform distribution from chaospy instead of uniform_integer.

--- easyvvuq/distributions/distributions.py
+++ easyvvuq/distributions/distributions.py
@@ -45,61 +45,6 @@ def options(choices):
             yield choice
 
 
-# A discrete distribution (integers only) for use with random seeds.
-class uniform_integer(Dist):
-    def __init__(self, lo, up):
-        """
-        Initializer.
-        """
-        Dist.__init__(self, lo=lo, up=up)
-        self.lo = lo
-        self.up = up
-
-    def cdf(self, x_data):
-        """
-        Cumulative distribution function.
-        """
-        return (x_data - self.lo) / (self.up - self.lo)
-
-    def bnd(self):
-        """
-        Lower and upper bounds.
-        """
-
-        return self.lo, self.up
-
-    def pdf(self, step=1e-07):
-        """
-        Probability density function.
-        """
-        return 1. / (self.up - self.lo)
-
-    def ppf(self, q_data):
-        """
-        Point percentile function.
-        """
-        return q_data * (self.up - self.lo) + self.lo
-
-    def sample(self, size=()):
-        """
-        Produces random integer values i, uniformly distributed on the closed
-        interval [lo, up], that is, distributed according to the discrete
-        probability function P(i/lo,up)=1/(up-lo).
-
-        Parameters
-        ----------
-        size (numpy.ndarray):
-            The size of the samples to generate.
-
-        Returns
-        -------
-        (numpy.ndarray):
-            Random samples with shape ``size``.
-        """
-
-        return np.random.randint(self.lo, self.up, size)
-
-
 # TODO: Convert this to a chaospy style distribution
 def custom_histogram(filename):
     """
