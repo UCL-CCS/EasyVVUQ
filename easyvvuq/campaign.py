2a8c2a7c5fa4823aa25e77aeaff83cd6eb7fdc81
Robert Sinclair <robert.sinclair.15@ucl.ac.uk>
2019-11-07 10:38:49 +0000

    collated dataframe format can now be one line per variable

--- easyvvuq/campaign.py
+++ easyvvuq/campaign.py
@@ -12,6 +12,7 @@ from easyvvuq import ParamsSpecification
 from easyvvuq.constants import default_campaign_prefix, Status
 from easyvvuq.data_structs import RunInfo, CampaignInfo, AppInfo
 from easyvvuq.sampling import BaseSamplingElement
+import pandas as pd
 
 __copyright__ = """
 
@@ -107,6 +108,8 @@ class Campaign:
         The current Decoder object being used, from the currently set app
     _active_app_collater: easyvvuq.collate.BaseCollationElement
         The current Collater object assigned to this campaign
+    _collated_dataframe_format: str
+        Is the dataframe output is 'one_row_per_rep' or 'one_row_per_var'
     _active_sampler: easyvvuq.sampling.BaseSamplingElement
         The currently set Sampler object
     _active_sampler_id: int
@@ -144,6 +147,7 @@ class Campaign:
         self._active_app_encoder = None
         self._active_app_decoder = None
         self._active_app_collater = None
+        self._collated_dataframe_format = 'one_row_per_rep'
 
         self._active_sampler = None
         self._active_sampler_id = None
@@ -677,6 +681,23 @@ class Campaign:
         info = {'num_collated': num_collated}
         self.log_element_application(self._active_app_collater, info)
 
+    def set_collated_dataframe_format(self, style):
+        """
+        Set the format of the results dataframe
+
+        Parameters
+        ----------
+        style : str
+            Controls how the collated results dataframe is organised
+            default 'one_row_per_rep'
+
+        Returns
+        -------
+        """
+        if style not in ['one_row_per_rep', 'one_row_per_var']:
+            raise Exception(style, 'is not a recognised dataframe output format')
+        self._collated_dataframe_format = style
+
     def get_collation_result(self):
         """
         Return dataframe containing all collated results
@@ -689,7 +710,32 @@ class Campaign:
             pandas dataframe
 
         """
-        return self._active_app_collater.get_collated_dataframe(self, self._active_app['id'])
+        collated_dataframe = self._active_app_collater.get_collated_dataframe(self, self._active_app['id'])
+
+        if self._collated_dataframe_format == 'one_row_per_rep':
+            pass
+
+        elif self._collated_dataframe_format == 'one_row_per_var':
+            # decoder may not have variable output_columns, does the database know?
+            output_columns = self._active_app_decoder.output_columns
+
+            # put output values into one column
+            output_stack = collated_dataframe[output_columns].stack()
+            output_stack.name = "Value"
+            # reset indicies and columns
+            output_stack = output_stack.reset_index(level=1)
+            output_stack.rename(columns={'level_1': 'Variable'}, inplace=True)
+
+            # Columns of dataframe that hold information about simulation but not output
+            sim_description_cols = [item for item in list(collated_dataframe.columns)
+                                    if item not in output_columns]
+            sim_description = collated_dataframe[sim_description_cols]
+
+            # merge sim_description with output in one column
+            collated_dataframe = sim_description.merge(output_stack,
+                                    left_index=True, right_index=True)
+
+        return collated_dataframe
 
     def apply_analysis(self, analysis):
         """Run the `analysis` element on the output of the last run collation.
