4e5b1bbe2cd361b6e75d25df4768bfc3bc7d6393
di73kuj2 <vytautas.jancauskas@lrz.de>
2019-12-04 12:52:22 +0100

    Added a command to build cannonsim

--- setup.py
+++ setup.py
@@ -1,17 +1,37 @@
 from os import path
 from setuptools import setup, find_packages
+import distutils
 import versioneer
+import subprocess
+import os
+
+class BuildCannonsimCommand(distutils.cmd.Command):
+    description = 'build cannonsim'
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        self.announce('Building cannonsim')
+        subprocess.check_call(['make'], cwd=os.path.abspath('./tests/cannonsim/src'))
 
 # read the contents of README file
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
+cmdclass = versioneer.get_cmdclass()
+cmdclass['build_cannonsim'] = BuildCannonsimCommand
+
 setup(
     name='easyvvuq',
 
     version=versioneer.get_version(),
-    cmdclass=versioneer.get_cmdclass(),
+    cmdclass=cmdclass,
 
     description=('Library to facilitate simple Verification, Validation and '
                  'Uncertainty Quantification of simulation codes'),
