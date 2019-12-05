e9e6882716339261b5f21fd193a62a9c28c8348a
di73kuj2 <vytautas.jancauskas@lrz.de>
2019-12-05 12:15:18 +0100

    A more sensible test

--- tests/test_jsondecoder.py
+++ tests/test_jsondecoder.py
@@ -3,4 +3,7 @@ import os
 
 
 def test_jsondecoder_basic():
-    decoder = JSONDecoder(os.path.join('jsondecoder', 'fredrik.json'), ['cfarc'])
+    decoder = JSONDecoder(os.path.join('jsondecoder', 'fredrik.json'), ['cfrac'])
+    run_info = {'run_dir' : 'tests'}
+    data = decoder.parse_sim_output(run_info)
+    assert(data['cfrac'] ==  0.24000000131541285)
