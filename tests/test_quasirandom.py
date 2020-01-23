1e15c128d170a76cd228eb0f6a4672363bc80511
Vytautas Jancauskas <unaudio@gmail.com>
2020-01-23 14:53:03 +0100

    Further pep8ing

--- tests/test_quasirandom.py
+++ tests/test_quasirandom.py
@@ -2,11 +2,11 @@ import easyvvuq as uq
 
 
 def test_lhc():
-    vary = {'a' : (-5, 3), 'b' : (2, 10)}
+    vary = {'a': (-5, 3), 'b': (2, 10)}
     sampler = uq.sampling.quasirandom.LHCSampler(vary, 5)
 
 
 def test_halton():
-    vary = {'a' : (-5, 3), 'b' : (2, 10)}
+    vary = {'a': (-5, 3), 'b': (2, 10)}
     sampler = uq.sampling.quasirandom.HaltonSampler(vary, 5)
 
