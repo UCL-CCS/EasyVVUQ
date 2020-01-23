f233e4dfbb4f7b7a54116f46b68347faa481c91e
Vytautas Jancauskas <unaudio@gmail.com>
2020-01-23 14:30:57 +0100

    Added HaltonSampler

--- easyvvuq/sampling/quasirandom.py
+++ easyvvuq/sampling/quasirandom.py
@@ -31,3 +31,30 @@ class LHCSampler(BaseSamplingElement, sampler_name='lhc_sampler'):
         return False
 
 
+class HaltonSampler(BaseSamplingElement, sampler_name='halton_sampler'):
+    def __init__(self, vary=None, order=1):
+        """
+            Expects dict of var names, and their ranges
+        """
+        self.vary = vary
+        self.samples = np.array([vary[param][0] + row * (vary[param][1] - vary[param][0])
+                                     for row, param in
+                                     zip(cp.distributions.sampler.sequences.halton.create_halton_samples(order=order, dim=len(vary.keys())), vary)])
+        self.order = order
+        self.dim = len(vary)
+        def sample_generator():
+            for sample in zip(*self.samples):
+                yield {key : value for key, value in zip(vary.keys(), sample)}
+        self.sample_generator = sample_generator()
+
+    def element_version(self):
+        return "0.1"
+
+    def is_finite(self):
+        return True
+
+    def __next__(self):
+        return next(self.sample_generator)
+
+    def is_restartable(self):
+        return False
--- tests/test_quasirandom.py
+++ tests/test_quasirandom.py
@@ -1,6 +1,12 @@
 import easyvvuq as uq
 
+
 def test_lhc():
     vary = {'a' : (-5, 3), 'b' : (2, 10)}
     sampler = uq.sampling.quasirandom.LHCSampler(vary, 5)
-    
+
+
+def test_halton():
+    vary = {'a' : (-5, 3), 'b' : (2, 10)}
+    sampler = uq.sampling.quasirandom.HaltonSampler(vary, 5)
+
