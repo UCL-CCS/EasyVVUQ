8183fd175f67528e08c07eefa1fb747f140ef112
Vytautas Jancauskas <unaudio@gmail.com>
2020-03-31 16:59:16 +0200

    more tests

--- tests/test_decoders_simple_csv.py
+++ tests/test_decoders_simple_csv.py
@@ -8,4 +8,8 @@ def test_simple_csv():
     df = decoder.parse_sim_output({'run_dir': os.path.join('tests', 'simple_csv')})
     assert(df['Step'][1] == 1)
     assert(df['Value'][5] == 25.950662)
-    
+    restart_dict = decoder.get_restart_dict()
+    assert(restart_dict['target_filename'] == 'test.csv')
+    assert(restart_dict['output_columns'] == ['Step', 'Value'])
+    assert(restart_dict['header'] == 0)
+    assert(decoder.sim_complete({'run_dir': os.path.join('tests', 'simple_csv')}))
