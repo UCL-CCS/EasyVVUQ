11b0f86cc88c8d4e166aabb0d853ca6da7ecdf08
Vytautas Jancauskas <unaudio@gmail.com>
2020-04-02 16:42:17 +0200

    add a multi_sampler fixture to test_sampling_sampler_of_samplers.py

--- tests/test_sampling_sampler_of_samplers.py
+++ tests/test_sampling_sampler_of_samplers.py
@@ -3,6 +3,16 @@ import pytest
 import unittest.mock as mock
 
 
+@pytest.fixture
+def multi_sampler():
+    sampler1 = mock.MagicMock()
+    sampler2 = mock.MagicMock()
+    sampler1.is_finite.return_value = True
+    sampler2.is_finite.return_value = True
+    multi_sampler = MultiSampler(sampler1, sampler2)
+    return multi_sampler
+
+
 def test_init_exceptions():
     # All samplers must be finite
     sampler1 = mock.MagicMock()
@@ -15,12 +25,11 @@ def test_init_exceptions():
         MultiSampler()
 
 
-def test_is_finite():
-    sampler1 = mock.MagicMock()
-    sampler2 = mock.MagicMock()
-    sampler1.is_finite.return_value = True
-    sampler2.is_finite.return_value = True
-    multi_sampler = MultiSampler(sampler1, sampler2)
+def test_element_version(multi_sampler):
+    assert(isinstance(multi_sampler.element_version(), str))
+
+
+def test_is_finite(multi_sampler):
     assert(multi_sampler.is_finite())
 
 
@@ -28,12 +37,7 @@ def test_iterator():
     pass
 
 
-def test_is_restartable():
-    sampler1 = mock.MagicMock()
-    sampler2 = mock.MagicMock()
-    sampler1.is_finite.return_value = True
-    sampler2.is_finite.return_value = True
-    multi_sampler = MultiSampler(sampler1, sampler2)
+def test_is_restartable(multi_sampler):
     assert(multi_sampler.is_restartable())
 
 
