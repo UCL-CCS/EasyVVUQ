92d6effebdb1aee85ef12ce1d77d0e080fa200f2
Vytautas Jancauskas <unaudio@gmail.com>
2020-04-03 15:10:46 +0200

    Test placeholder in test_sampling_sampler_of_samplers.py

--- tests/test_sampling_sampler_of_samplers.py
+++ tests/test_sampling_sampler_of_samplers.py
@@ -34,12 +34,26 @@ def test_is_finite(multi_sampler):
 
 
 def test_iterator():
-    pass
+    def popper(values):
+        if len(values):
+            values.pop()
+        else:
+            raise StopIteration
+    sampler1 = mock.MagicMock()
+    values1 = [3, 2, 1]
+    sampler1.__next__.return_value = values1[-1]
+    sampler1.__next__.side_effect = popper(values1)
+    sampler2 = mock.MagicMock()
+    values2 = [3, 2, 1]
+    sampler2.__next__.return_value = values2[-1]
+    sampler2.__next__.side_effect = popper(values2)
+    sampler1.is_finite.return_value = True
+    sampler2.is_finite.return_value = True
 
 
 def test_is_restartable(multi_sampler):
     assert(multi_sampler.is_restartable())
 
 
-def test_get_restart_dict():
-    pass
+def test_get_restart_dict(multi_sampler):
+    assert(isinstance(multi_sampler.get_restart_dict(), dict))
