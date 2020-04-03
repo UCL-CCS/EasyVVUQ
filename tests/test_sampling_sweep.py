9c5e3de5ff0bbcd8792a44e3443cab04963a9b57
Vytautas Jancauskas <unaudio@gmail.com>
2020-04-03 16:21:06 +0200

    Added test_sampling_sweep.py

---
+++ tests/test_sampling_sweep.py
@@ -0,0 +1,34 @@
+from easyvvuq.sampling.sweep import BasicSweep
+import pytest
+
+
+def test_wrap_iterable():
+    pass
+
+
+def test_init():
+    pass
+
+
+def test_init_exceptions():
+    pass
+
+
+def test_element_version():
+    pass
+
+
+def test_is_finite():
+    pass
+
+
+def test_iter():
+    pass
+
+
+def test_is_restartable():
+    pass
+
+
+def test_get_restart_dict():
+    pass
