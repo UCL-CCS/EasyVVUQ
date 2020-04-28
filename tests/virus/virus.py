cda73033ab703afc95b6bce9e0c4f19c271f2498
Vytautas Jancauskas <unaudio@gmail.com>
2020-04-28 10:31:05 +0200

    Added a copyright statement to the virus example

--- tests/virus/virus.py
+++ tests/virus/virus.py
@@ -6,6 +6,29 @@ import sys
 import json
 
 
+__copyright__ = """
+
+    Copyright 2020 Vytautas Jancauskas
+
+    This file is part of EasyVVUQ
+
+    EasyVVUQ is free software: you can redistribute it and/or modify
+    it under the terms of the Lesser GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    EasyVVUQ is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    Lesser GNU General Public License for more details.
+
+    You should have received a copy of the Lesser GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+"""
+__license__ = "LGPL"
+
+
 class Population:
     def __init__(self, grid_size, n, duration, mortality):
         self.grid_size = grid_size
@@ -24,7 +47,7 @@ class Population:
                 if i != j:
                     if ((x1 == x2).all() and
                         self.ill[j] > 0 and
-                            self.ill[i] == 0 and not self.immune[i]):
+                        self.ill[i] == 0 and not self.immune[i]):
                         self.ill[i] = self.duration
                         self.immune[i] = 1
         to_delete = []
