5847de0ad740927d1952f8598fcbac45947a9c7f
Vytautas Jancauskas <unaudio@gmail.com>
2020-05-26 15:47:53 +0200

    Break down the validate similarity classes

--- easyvvuq/comparison/validate.py
+++ easyvvuq/comparison/validate.py
@@ -31,26 +31,8 @@ __license__ = "LGPL"
 
 class ValidateSimilarity(BaseComparisonElement):
 
-    def __init__(self, metric="H"):
-        """Compare Similarities between two QoI distributions.
-        The comparison can use one of the following metrics:
-            - Hellinger.
-            - Jensen Shannon (symmetrized verion of Kullback-Leibler divergence),
-            - Wasserstein 1 and Wasserstein 2 (called also Earth mover's).
-        Lower distance means hight similarity.
-
-        Parameters
-        ----------
-        metric : str, optional
-           supported metrics:
-           Hellinger (H), Jensen Shannon (JS), Wasserstein 1 (W1) and  Wasserstein 2 (W2).
-           default: Hellinger (H).
-        """
-
-        if metric not in ["H", "JS", "W1", "W2"]:
-            raise RuntimeError("Validate_Similarity: Unknown distance name.")
-
-        self._metric = metric
+    def __init__(self):
+        pass
 
     def element_name(self):
         return "validate_similarity"
@@ -58,25 +40,8 @@ class ValidateSimilarity(BaseComparisonElement):
     def element_version(self):
         return "0.1"
 
-    def set_metric(self, metric):
-        """
-        Parameters
-        ----------
-        metric : str
-            supported metrics:
-                H  : Hellinger
-                JS : Jensen Shannon
-                W1 : Wasserstein 1
-                W2 : Wasserstein 2
-        """
-
-        if metric not in ["H", "JS", "W1", "W2"]:
-            raise RuntimeError("Validate_Similarity: Unknown distance name.")
-
-        self._metric = metric
-
-    def get_metric(self):
-        return self._metric
+    def dist(p, q):
+        raise NotImplemented
 
     def compare(self, dataframe1, dataframe2):
         """Perform comparaison between dataframe1 and dataframe2, two lists of:
@@ -91,26 +56,6 @@ class ValidateSimilarity(BaseComparisonElement):
         if len(dataframe1) != len(dataframe2):
             raise RuntimeError("Input dataframe sizes are not equal")
 
-        # Compute the distance
-        def dist(p, q):
-            if self._metric == "H":
-                p /= p.sum()
-                q /= q.sum()
-                return np.sqrt(1. - np.sqrt(p * q).sum())
-
-            if self._metric == "JS":
-                p /= p.sum()
-                q /= q.sum()
-                m = 0.5 * (p + q)
-                div = 0.5 * (st.entropy(p, m) + st.entropy(q, m))
-                return np.sqrt(div / np.log(2))
-
-            if self._metric == "W1":
-                return st.wasserstein_distance(p, q)
-
-            if self._metric == "W2":
-                return st.energy_distance(p, q)
-
         # output
         shape = np.shape(dataframe1)
         if len(shape) == 2:
@@ -118,14 +63,40 @@ class ValidateSimilarity(BaseComparisonElement):
             for i in range(len(dataframe1)):
                 p1 = np.array(dataframe1[i])
                 p2 = np.array(dataframe2[i])
-                d = dist(p1, p2)
+                d = self.dist(p1, p2)
                 results.append(d)
         else:
             p1 = np.array(dataframe1)
             p2 = np.array(dataframe2)
-            results = dist(p1, p2)
+            results = self.dist(p1, p2)
 
         return results
+
+
+class ValidateSimilarityHellinger(ValidateSimilarity):
+    def dist(p, q):
+        p /= p.sum()
+        q /= q.sum()
+        return np.sqrt(1. - np.sqrt(p * q).sum())        
+
+
+class ValidateSimilarityShannonJenson(ValidateSimilarity):
+    def dist(p, q):
+        p /= p.sum()
+        q /= q.sum()
+        m = 0.5 * (p + q)
+        div = 0.5 * (st.entropy(p, m) + st.entropy(q, m))
+        return np.sqrt(div / np.log(2))
+
+
+class ValidateSimilarityWasserstein1(ValidateSimilarity):
+    def dist(p, q):
+        return st.wasserstein_distance(p, q)
+
+
+class ValidateSimilarityWasserstein2(ValidateSimilarity):
+    def dist(p, q):
+        return st.energy_distance(p, q)
 
 
 class ValidateCompatability(BaseComparisonElement):
