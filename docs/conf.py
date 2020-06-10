967cb18bce05678ba1c257115023e88dbd59909e
Diana Suleimenova <diana.suleimenova@brunel.ac.uk>
2020-06-10 18:33:38 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -85,4 +85,4 @@ html_theme_options = {
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-html_logo = "../_static/circle-logo.svg"
+html_logo = "../_static/easyvvuq_logo.png"
