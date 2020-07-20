8241b77280eb37063407db06665b2e4b5ea1f892
Vytautas Jancauskas <unaudio@gmail.com>
2020-07-20 16:30:04 +0200

    Another typo

--- tests/test_encoders_copy_encoder.py
+++ tests/test_encoders_copy_encoder.py
@@ -11,7 +11,7 @@ def test_copy_encoder():
 def test_copy_encoder_encode(tmpdir):
     filename1 = os.path.join(tmpdir, 'test1.txt')
     filename2 = 'test2.txt'
-    open(os.path.join(filename1, 'a')).close()
+    open(filename1, 'a').close()
     encoder = CopyEncoder(filename1, filename2)
     with pytest.raises(RuntimeError):
         encoder.encode({}, 'axcd')
