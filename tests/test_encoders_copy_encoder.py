89b612c1c96583aea6019d15b8fc8c15ddd41b86
Vytautas Jancauskas <unaudio@gmail.com>
2020-07-20 16:59:36 +0200

    tmp_path instead of tmpdir

--- tests/test_encoders_copy_encoder.py
+++ tests/test_encoders_copy_encoder.py
@@ -8,12 +8,12 @@ def test_copy_encoder():
         CopyEncoder('axcd', 'axcd')
 
 
-def test_copy_encoder_encode(tmpdir):
-    filename1 = os.path.join(tmpdir, 'test1.txt')
+def test_copy_encoder_encode(tmp_path):
+    filename1 = os.path.join(tmp_path, 'test1.txt')
     filename2 = 'test2.txt'
     open(filename1, 'a').close()
     encoder = CopyEncoder(filename1, filename2)
     with pytest.raises(RuntimeError):
         encoder.encode({}, 'axcd')
-    encoder.encode(tmpdir)
-    assert(os.path.isfile(os.path.join(tmpdir, filename2)))
+    encoder.encode(tmp_path)
+    assert(os.path.isfile(os.path.join(tmp_path, filename2)))
