c732eeb52b2695d404eed34a68c7500f8672581a
Vytautas Jancauskas <unaudio@gmail.com>
2020-07-20 15:48:25 +0200

    Added test_encoders_copy_encoder.py

---
+++ tests/test_encoders_copy_encoder.py
@@ -0,0 +1,18 @@
+import pytest
+from easyvvuq.encoders.copy_encoder import CopyEncoder
+
+
+def test_copy_encoder():
+    with pytest.raises(RuntimeError):
+        CopyEncoder('axcd', 'axcd')
+
+
+def test_copy_encoder_encode(tmpdir):
+    filename1 = os.path.join(tmpdir, 'test1.txt')
+    filename2 = 'test2.txt'
+    open(os.path.join(filename, 'a')).close()
+    encoder = CopyEncoder(filename1, filename2)
+    with pytest.raises(RuntimeError):
+        encoder.encode({}, 'axcd')
+    encoder.encode(tmpdir)
+    assert(os.path.isfile(os.path.join(tmpdir, filename2)))
