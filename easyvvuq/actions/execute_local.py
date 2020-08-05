a9f538db9a56a810168907df57fccc94c345f4ec
Vytautas Jancauskas <unaudio@gmail.com>
2020-08-05 15:07:44 +0200

    implemented ExecuteLocalV2

--- easyvvuq/actions/execute_local.py
+++ easyvvuq/actions/execute_local.py
@@ -4,6 +4,7 @@
 import os
 import sys
 import logging
+import subprocess
 from . import BaseAction
 
 __copyright__ = """
@@ -89,3 +90,56 @@ class ExecuteLocal(BaseAction):
         if result != 0:
             sys.exit(f'Non-zero exit code from command "{full_cmd}"\n')
         return ActionStatusLocal()
+
+
+class ActionStatusLocalV2():
+    def __init__(self, popen_object):
+        self.popen_object = popen_object
+        self.ret = None
+
+    def finished(self):
+        """Returns true if action is finished. In this case if calling poll on
+        the popen object returns a non-None value.
+        """
+        ret = self.popen_object.poll()
+        if ret is not None:
+            self.ret = ret
+            return True
+        else:
+            return False
+
+    def finalise(self):
+        """Performs clean-up if necessary. In this case it isn't. I think.
+        """
+        return None
+
+    def succeeded(self):
+        """Will return True if the process finished successfully.
+        It judges based on the return code and will return False
+        if that code is not zero.
+        """
+        if self.ret != 0:
+            return False
+        else:
+            return True
+
+
+class ExecuteLocalV2(ExecuteLocal):
+    """An improvement over ExecuteLocal that uses Popen and provides the non-blocking
+    execution that allows you to track progress. In line with other Action classes in EasyVVUQ.
+    """
+    def act_on_dir(self, target_dir):
+        """
+        Executes `self.run_cmd` in the shell in `target_dir`.
+
+        target_dir : str
+            Directory in which to execute command.
+
+        """
+
+        if self.interpreter is None:
+            full_cmd = self.run_cmd.split()
+        else:
+            full_cmd = [self.interpreter] + self.run_cmd.split()
+        return ActionStatusLocalV2(subprocess.Popen(full_cmd, cwd=target_dir))
+
