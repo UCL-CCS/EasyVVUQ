5872710b4401c59e4d0ad4d798daf045ed72b17f
Vytautas Jancauskas <unaudio@gmail.com>
2020-08-17 16:18:35 +0200

    Added some docstrings

--- easyvvuq/actions/execute_slurm.py
+++ easyvvuq/actions/execute_slurm.py
@@ -1,4 +1,5 @@
-"""Provides element to execute a shell command in a given directory.
+"""Provides a simple action element for interacting with a SLURM job. This lets you
+execute your simulation on a SLURM cluster.
 """
 
 import os
@@ -32,6 +33,19 @@ __license__ = "LGPL"
 logger = logging.getLogger(__name__)
 
 class ActionStatusSLURM():
+    """An ActionStatus to track the execution of a SLURM job.
+    
+    Parameters
+    ----------
+    script: str
+        The body of the script. Will be written to a file 
+        relative to target_dir named script_name.
+    script_name: str
+        Name of the script file. This will be written to. 
+        So beware of overwriting issues.
+    target_dir: str
+        Name of the execution directory for this job.
+    """
     def __init__(self, script, script_name, target_dir):
         self.script = script
         self.script_name = script_name
@@ -39,12 +53,16 @@ class ActionStatusSLURM():
         self._started = False
 
     def start(self):
+        """Start the SLURM job.
+        """
         result = subprocess.run(['sbatch', self.script_name], cwd=self.target_dir, check=True)
         stdout = result.stdout.decode('utf-8')
         self.job_id = re.findall(r'\d+', stdout)[0]
         self._started = True
 
     def started(self):
+        """Returns True if the job has started.
+        """
         return self._started
 
     def finished(self):
@@ -79,16 +97,18 @@ class ActionStatusSLURM():
 
 
 class ExecuteSLURM(ExecuteLocal):
-    """An improvement over ExecuteLocal that uses Popen and provides the non-blocking
-    execution that allows you to track progress. In line with other Action classes in EasyVVUQ.
+    """An improvement over ExecuteLocal that uses Popen and provides the
+    non-blocking execution that allows you to track progress. In line
+    with other Action classes in EasyVVUQ.
 
     Parameters
     ----------
     template_script: str
        filename for the template slurm script
 
-    variable: str
-       a string in the template script that will be replaced with the target_dir
+    variable: str a string in the template script that will be
+       replaced with the target_dir
+
     """
 
     def __init__(self, template_script, variable):
@@ -105,4 +125,5 @@ class ExecuteSLURM(ExecuteLocal):
             Directory in which to execute command.
 
         """
-        return ActionStatusSLURM(self.template.replace(self.variable, target_dir), target_dir)
+        return ActionStatusSLURM(
+            self.template.replace(self.variable, target_dir), target_dir)
