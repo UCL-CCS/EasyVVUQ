ab24a95b52dd7ea7120f6ff44a5b792454a59c25
Vytautas Jancauskas <unaudio@gmail.com>
2020-08-19 14:39:42 +0200

    removed all commented out stuff

--- tests/test_actions_execute_slurm.py
+++ tests/test_actions_execute_slurm.py
@@ -27,38 +27,3 @@ def test_action_status_slurm():
     status.start()
     assert(status.job_id == '65541')
     assert(status.started())
-    
-
-# def test_execute_kubernetes():
-#     action = execute_kubernetes.ExecuteKubernetes(
-#         'tests/kubernetes/epidemic.yaml', ['epidemic.json'], 'out.csv')
-#     action.act_on_dir('tests/kubernetes')
-
-
-# def test_action_status_kubernetes():
-#     api = MagicMock()
-#     pod_name = 'test'
-#     config_names = [('a', 'b'), ('c', 'd')]
-#     namespace = 'test_namespace'
-#     outfile = 'test.csv'
-#     status = ActionStatusKubernetes(
-#         api, {'metadata': {'name': 'test'}},
-#         config_names, namespace, outfile)
-#     resp = MagicMock()
-#     resp.status.phase = 'Pending'
-#     api.read_namespaced_pod.return_value = resp
-#     assert(not status.finished())
-#     with pytest.raises(RuntimeError):
-#         status.finalise()
-#     assert(not status.succeeded())
-#     resp.status.phase = 'Succeeded'
-#     assert(status.finished())
-#     api.read_namespaced_pod_log.return_value = 'testing'
-#     status.finalise()
-#     assert(os.path.isfile('test.csv'))
-#     with open('test.csv', 'r') as fd:
-#         assert(fd.read() == 'testing')
-#     os.remove('test.csv')
-#     assert(api.delete_namespaced_config_map.called_with('b', 'test_namespace'))
-#     assert(api.delete_namespaced_config_map.called_with('d', 'test_namespace'))
-#     assert(api.delete_namespaced_pod.called_with('test', 'test_namespace'))
