c1e92235a0f7322db0094098f74e9bb6b7328f98
Vytautas Jancauskas <unaudio@gmail.com>
2020-08-27 17:03:32 +0200

    Added a basic qmc sampler init test

--- tests/test_sampling_qmc.py
+++ tests/test_sampling_qmc.py
@@ -1,8 +1,10 @@
 import pytest
+import chaospy as cp
 from easyvvuq.sampling import QMCSampler
 
 
-def test_init(self):
+def test_init():
     vary = {'a': cp.Uniform(-5, 3), 'b': cp.Uniform(2, 10)}
     with pytest.raises(RuntimeError):
-        QMCSampler(vary)
+        QMCSampler({}, 100)
+    
