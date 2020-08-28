f69edb820c8b721aec297710dce32056225a592e
Robin Richardson <raarichardson@gmail.com>
2020-08-28 17:59:38 +0200

    Check whether encoder has actually exited successfully before trying to execute canonsim...

--- tests/test_worker.py
+++ tests/test_worker.py
@@ -1,6 +1,7 @@
 import easyvvuq as uq
 import chaospy as cp
 import os
+import sys
 import pytest
 from easyvvuq.constants import default_campaign_prefix, Status
 from pprint import pprint
@@ -136,9 +137,16 @@ def test_worker(tmpdir):
             run_id
         ]
         encoder_path = os.path.realpath(os.path.expanduser("easyvvuq/tools/external_encoder.py"))
-        subprocess.run(['python3', encoder_path] + enc_args)
-        subprocess.run([CANNONSIM_PATH, "in.cannon", "output.csv"], cwd=run_data['run_dir'])
+        try:
+            subprocess.run(['python3', encoder_path] + enc_args, check=True)
+        except subprocess.CalledProcessError as e:
+            sys.exit(f"Failed during encoding of run: f{e}")
 
+        try:
+            subprocess.run([CANNONSIM_PATH, "in.cannon", "output.csv"], cwd=run_data['run_dir'], check=True)
+        except subprocess.CalledProcessError as e:
+            sys.exit(f"Failed during execution of run: f{e}")
+        
         my_campaign.campaign_db.set_run_statuses([run_id], Status.ENCODED)  # see note further down
 
     # Encode and execute. Note to call function for all runs with status NEW (and not ENCODED)
