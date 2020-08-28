ec3f18a965119f3cdab3440910143198c54d2610
Vytautas Jancauskas <unaudio@gmail.com>
2020-08-28 11:08:50 +0200

    test update

--- tests/test_sampling_qmc.py
+++ tests/test_sampling_qmc.py
@@ -7,4 +7,5 @@ def test_init():
     vary = {'a': cp.Uniform(-5, 3), 'b': cp.Uniform(2, 10)}
     with pytest.raises(RuntimeError):
         QMCSampler({}, 100)
-    
+    with pytest.raises(RuntimeError):
+        QMCSampler([], 100)
