092405e82e34af0f4f628fc506820feb91b6da12
Vytautas Jancauskas <unaudio@gmail.com>
2020-09-02 16:29:23 +0200

    tests

--- tests/test_analysis_qmc_analysis.py
+++ tests/test_analysis_qmc_analysis.py
@@ -3,6 +3,7 @@ from easyvvuq.analysis.qmc_analysis import QMCAnalysis
 from easyvvuq.sampling.qmc import QMCSampler
 import chaospy as cp
 import pandas as pd
+import numpy as np
 
 def test_analyse():
     # we will use a simple model where there are two uniformly distributed
@@ -19,7 +20,6 @@ def test_analyse():
     df = pd.DataFrame(samples, columns=['run_id', 'a', 'b', 'a+b'])
     analysis = QMCAnalysis(sampler)
     results = analysis.analyse(df)
-    print(results)
 
 if __name__ == '__main__':
     test_analyse()
