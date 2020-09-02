5cb7bf8dc03c0d113d38c6fe79a1ad348ddb5e19
Vytautas Jancauskas <unaudio@gmail.com>
2020-09-02 14:40:43 +0200

    changes

--- tests/test_analysis_qmc_analysis.py
+++ tests/test_analysis_qmc_analysis.py
@@ -4,11 +4,10 @@ from easyvvuq.sampling.qmc import QMCSampler
 import chaospy as cp
 import pandas as pd
 
-
 def test_analyse():
     # we will use a simple model where there are two uniformly distributed
     # parameters and the model evaluates to the sum of them
-    vary = {
+    vary =  {
         "a": cp.Uniform(0.0, 1.0),
         "b": cp.Uniform(0.0, 1.0)
     }
@@ -20,7 +19,7 @@ def test_analyse():
     df = pd.DataFrame(samples, columns=['run_id', 'a', 'b', 'a+b'])
     analysis = QMCAnalysis(sampler)
     results = analysis.analyse(df)
-
+    print(results)
 
 if __name__ == '__main__':
     test_analyse()
