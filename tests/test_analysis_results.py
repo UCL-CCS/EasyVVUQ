7668bdeb2061f73f9a5083eee58ad22accd4121e
Vytautas Jancauskas <unaudio@gmail.com>
2020-09-18 11:11:24 +0200

    complete the test for keys_to_tuples

--- tests/test_analysis_results.py
+++ tests/test_analysis_results.py
@@ -2,3 +2,9 @@ from easyvvuq.analysis.results import AnalysisResults
 
 def test_keys_to_tuples():
     assert(AnalysisResults._keys_to_tuples({}) == {})
+    assert(AnalysisResults._keys_to_tuples(
+        {'a': 1, 'b': 2}) == {('a', 0): 1, ('b', 0): 2})
+    assert(AnalysisResults._keys_to_tuples(
+        {('a', 0): 1, ('b', 0): 2}) == {('a', 0): 1, ('b', 0): 2})
+    assert(AnalysisResults._keys_to_tuples(
+        {('a', 0): 1, 'b': 2}) == {('a', 0): 1, ('b', 0): 2})
