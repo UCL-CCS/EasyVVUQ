9df36136987c4b916554707f0013e20ebceb275a
Vytautas Jancauskas <unaudio@gmail.com>
2020-11-02 16:18:23 +0100

    fill out the get_results test

--- tests/test_db.py
+++ tests/test_db.py
@@ -145,3 +145,7 @@ def test_collation(campaign):
     campaign.store_results('test', results)
     result = campaign.get_results('test')
     assert(isinstance(result, pd.DataFrame))
+    assert(list(result.columns) == [('a', 0), ('b', 0), ('c', 0), ('c', 1)])
+    assert(list(result.iloc[100].values) == [1, 100, 101, 102])
+    assert(result.count()[0] == 1010)
+
