9ce494501a70158656219ac96c9c90ff0952fbdf
Vytautas Jancauskas <unaudio@gmail.com>
2020-11-18 17:00:42 +0100

    some updates

--- easyvvuq/analysis/results.py
+++ easyvvuq/analysis/results.py
@@ -144,9 +144,9 @@ class AnalysisResults:
         a dictionary or an array
         """
         assert(not ((qoi is None) and (input_ is not None)))
-        if qoi not in self.qois:
+        if (qoi is not None) and (qoi not in self.qois):
             raise RuntimeError('no such qoi in this analysis')
-        if input_ not in self.inputs:
+        if (input_ is not None) and (input_ not in self.inputs):
             raise RuntimeError('no such input variable in this analysis')
         try:
             if input_ is None:
@@ -183,9 +183,9 @@ class AnalysisResults:
         a dictionary or an array
         """
         assert(not ((qoi is None) and (input_ is not None)))
-        if qoi not in self.qois:
+        if (qoi is not None) and (qoi not in self.qois):
             raise RuntimeError('no such qoi in this analysis')
-        if input_ not in self.inputs:
+        if (input_ is not None) and (input_ not in self.inputs):
             raise RuntimeError('no such input variable in this analysis')
         if input_ is None:
             if qoi is None:
