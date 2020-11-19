b2d4ebacf44d5576176d10071797da80de03ce6c
Vytautas Jancauskas <unaudio@gmail.com>
2020-11-19 12:28:17 +0100

    added a test to check vector qoi resutls

--- tests/test_sc_analysis_results.py
+++ tests/test_sc_analysis_results.py
@@ -42,12 +42,42 @@ def data():
 
 
 @pytest.fixture
+def data_vectors():
+    np.random.seed(10000000)
+    vary = {
+        "x1": cp.Uniform(0.0, 1.0),
+        "x2": cp.Uniform(0.0, 1.0)
+    }
+    sampler = uq.sampling.SCSampler(vary)
+    data = {('run_id', 0): [], ('x1', 0): [], ('x2', 0): [],
+            ('g', 0): [], ('g', 1): [], ('g', 2): []}
+    for run_id, sample in enumerate(sampler):
+        data[('run_id', 0)].append(run_id)
+        data[('x1', 0)].append(sample['x1'])
+        data[('x2', 0)].append(sample['x2'])
+        data[('g', 0)].append(sample['x1'])
+        data[('g', 1)].append(sample['x2'])
+        data[('g', 2)].append(sample['x1'] + sample['x2'])
+    df = pd.DataFrame(data)
+    return sampler, df
+
+
+@pytest.fixture
 def results(data):
     # Post-processing analysis
-    mc_sampler, df = data
-    analysis = uq.analysis.SCAnalysis(sampler=mc_sampler, qoi_cols=['f'])
+    sampler, df = data
+    analysis = uq.analysis.SCAnalysis(sampler=sampler, qoi_cols=['f'])
     results = analysis.analyse(df)
     return results
+
+
+@pytest.fixture
+def results_vectors(data_vectors):
+    # Post-processing analysis
+    sampler, df = data_vectors
+    analysis = uq.analysis.SCAnalysis(sampler=sampler, qoi_cols=[('g', 0), ('g', 1), ('g', 2)])
+    results = analysis.analyse(df)
+    return results    
 
 
 def test_results(results):
@@ -86,3 +116,13 @@ def test_describe(results):
         'std': 0.8184617581393419,
         'var': 0.6698796495365424
     }))
+
+
+def test_vectors(results_vectors):
+    assert(results_vectors.sobols_first(('g', 0), 'x1') == pytest.approx(1.0))
+    assert(results_vectors.sobols_first(('g', 0), 'x2') == pytest.approx(0.0))
+    assert(results_vectors.sobols_first(('g', 1), 'x1') == pytest.approx(0.0))
+    assert(results_vectors.sobols_first(('g', 1), 'x2') == pytest.approx(1.0))
+    assert(results_vectors.sobols_first(('g', 2), 'x1') == pytest.approx(0.5))
+    assert(results_vectors.sobols_first(('g', 2), 'x2') == pytest.approx(0.5))
+
