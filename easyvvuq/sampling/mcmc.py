77a0a7fffefbdb4ced27896bf2a6f1f5e2385887
Vytautas Jancauskas <unaudio@gmail.com>
2021-01-12 13:44:18 +0100

    added mcmc sampler stub

---
+++ easyvvuq/sampling/mcmc.py
@@ -0,0 +1,26 @@
+from .base import BaseSamplingElement, Vary
+import numpy as np
+import chaospy as cp
+
+
+class MCMCSampler(BaseSamplingElement, sampler_name='mcmc_sampler'):
+    def __init__(self, vary):
+        self.vary = Vary(vary)
+
+    def element_version(self):
+        return "0.1"
+
+    def is_finite(self):
+        return True
+
+    def n_samples(self):
+        return 1
+
+    def __next__(self):
+        return 0.0
+
+    def is_restartable(self):
+        return True
+
+    def get_restart_dict(self):
+        return {"vary" : self.vary.serialize()}
