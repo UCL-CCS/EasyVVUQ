a133bef7013fc5992c0e7267aa4445ef5a470840
Vytautas Jancauskas <unaudio@gmail.com>
2021-02-01 15:47:38 +0100

    Update qmc_analysis.py

--- easyvvuq/analysis/qmc_analysis.py
+++ easyvvuq/analysis/qmc_analysis.py
@@ -149,9 +149,8 @@ class QMCAnalysis(BaseAnalysisElement):
         Returns
         -------
         dict : 
-            A dictionary with the QoI names as keys. Each samples[qoi_name]
-        is a list of code evaluations.
-
+            A dictionary with the QoI names as keys. 
+        Each element is a list of code evaluations.
         """
         samples = {k: [] for k in self.qoi_cols}
         for run_id in data_frame['run_id'].squeeze().unique():
