e099615ff433138bc4a321731c47e75d14ca2ad6
Vytautas Jancauskas <unaudio@gmail.com>
2021-02-23 13:52:49 +0100

    updated mcmc tutorial

--- easyvvuq/sampling/replica_sampler.py
+++ easyvvuq/sampling/replica_sampler.py
@@ -23,15 +23,15 @@ class ReplicaSampler(BaseSamplingElement, sampler_name='replica_sampler'):
     sampler : an instance of a class derived from  BaseSamplingElement
         a finite sampler to loop over
 
-    ensemble_col : string
-        a parameter name for the ensemble id
+    replica_col : string
+        a parameter name for the replica id
     """
 
-    def __init__(self, sampler, ensemble_col='ensemble'):
+    def __init__(self, sampler, replica_col='replica_id'):
         if not sampler.is_finite():
             raise RuntimeError("Replica sampler only works with finite samplers")
         self.sampler = sampler
-        self.ensemble_col = ensemble_col
+        self.replica_col = replica_col
         self.history = []
         for sample in sampler:
             self.history.append(sample)
@@ -50,7 +50,7 @@ class ReplicaSampler(BaseSamplingElement, sampler_name='replica_sampler'):
 
     def __next__(self):
         params = dict(next(self.cycle))
-        params[self.ensemble_col] = self.counter
+        params[self.replica_col] = self.counter
         self.counter = (self.counter + 1) % self.size
         return params
 
