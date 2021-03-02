151230abd3938ad6870da62c36a01810e263f098
Vytautas Jancauskas <unaudio@gmail.com>
2021-03-02 12:27:51 +0100

    some basic plotting routines in mcmc analysis results

--- easyvvuq/analysis/mcmc.py
+++ easyvvuq/analysis/mcmc.py
@@ -1,6 +1,7 @@
 """Analysis element for Markov Chain Monte Carlo (MCMC).
 """
 import chaospy as cp
+import pandas as pd
 from .base import BaseAnalysisElement
 from .results import AnalysisResults
 
@@ -14,18 +15,20 @@ class MCMCAnalysisResults(AnalysisResults):
     qoi: ndarray of shape (nsamples, 1)
     """
 
-    def __init__(self, samples, qoi):
-        self.samples = samples
-        self.qoi = qoi
+    def __init__(self, chains):
+        self.chains = chains
 
-    def distribution(self):
-        """Returns the KDE estimation of the distribution.
+    def plot_hist(self, input_parameter, chain, skip=0):
+        import matplotlib.pyplot as plt
+        plt.hist(self.chains[chain][input_parameter].iloc[skip:], 20)
 
-        Returns
-        -------
-        ChaosPy distribution
-        """
-        return cp.GaussianKDE(self.samples.values.T)
+    def plot_chain(self, input_parameter, chain=None):
+        import matplotlib.pyplot as plt
+        if chain is None:
+            for chain in self.chains:
+                plt.plot(self.chains[chain][input_parameter])
+        else:
+            plt.plot(self.chains[chain][input_parameter])
 
 
 class MCMCAnalysis(BaseAnalysisElement):
@@ -52,6 +55,17 @@ class MCMCAnalysis(BaseAnalysisElement):
         return "0.1"
 
     def analyse(self, df):
-        samples = df[self.sampler.inputs]
-        qoi = df[self.sampler.qoi]
-        return MCMCAnalysisResults(samples, qoi)
+        chains = dict([(chain_id, []) for chain_id in df[('chain_id', 0)].unique()])
+        for chain in chains:
+            chain_values = df[df[('chain_id', 0)] == chain]
+            values = chain_values.groupby(('iteration', 0)).apply(lambda x: x.mean())
+            indexes = values.index.values
+            for a, b in zip(indexes[:-1], indexes[1:]):
+                chains[chain] += [values.loc[a][self.sampler.inputs].to_dict()] * (b - a)
+        for chain in chains:
+            tmp = dict([(input_, []) for input_ in chains[chain][0]])
+            for row in chains[chain]:
+                for input_ in chains[chain][0]:
+                    tmp[input_].append(row[input_])
+            chains[chain] = pd.DataFrame(tmp)
+        return MCMCAnalysisResults(chains)
