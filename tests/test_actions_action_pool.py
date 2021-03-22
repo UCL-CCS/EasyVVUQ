15a93ef2282a75b7c95f70d2ef61fd24900a4e8d
Vytautas Jancauskas <unaudio@gmail.com>
2021-03-22 12:44:38 +0100

    testing action pool

--- tests/test_actions_action_pool.py
+++ tests/test_actions_action_pool.py
@@ -1,11 +1,23 @@
 import pytest
 import time
+import chaospy as cp
 from unittest.mock import MagicMock
-from easyvvuq.actions import ActionPool
+from easyvvuq.actions import ActionPool, ExecutePython, Actions
+from easyvvuq import Campaign
+from easyvvuq.sampling import RandomSampler
 
+@pytest.fixture
+def campaign():
+    def model(params):
+        return {'y' : params['x'] + 1}
+    actions = Actions(ExecutePython(model))
+    sampler = RandomSampler({'x': cp.Uniform(0, 1)})
+    campaign = Campaign('test', {'x': {'default' : 0}}, actions)
+    campaign.set_sampler(sampler)
+    return campaign
 
-def action_pool(sequential):
-    campaign = MagicMock()
-    actions = MagicMock()
-    inits = [MagicMock(), MagicMock(), MagicMock()]
-    return ActionPool(campaign, actions, inits, sequential=sequential)
+def test_action_pool_start(campaign):
+    action_pool = campaign.execute(nsamples=3)
+    assert(len(action_pool.futures) == 3)
+    action_pool.collate()
+    assert(len(action_pool.campaign.get_collation_result()) == 3)
