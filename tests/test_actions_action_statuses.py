d849b676f06983e8a58ab78367fa88f4b5e270d4
Vytautas Jancauskas <unaudio@gmail.com>
2021-03-29 15:02:14 +0200

    added test_actions_action_statuses.py

---
+++ tests/test_actions_action_statuses.py
@@ -0,0 +1,14 @@
+import pytest
+import time
+from unittest.mock import MagicMock
+from easyvvuq.actions import ActionPool
+
+@pytest.fixture(scope="module", params=[False])
+def action_pool():
+    campaign = MagicMock()
+    actions = MagicMock()
+    inits = [MagicMock(), MagicMock(), MagicMock()]
+    return ActionPool(campaign, actions, inits, sequential=False)
+
+def test_action_pool_start(action_pool):
+    pass
