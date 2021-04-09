fdc31fe1f8e1726899bb27f9aa1990eebf36fa98
Vytautas Jancauskas <unaudio@gmail.com>
2021-04-09 13:38:39 +0200

    remove some obsolete stuff from base_element

--- easyvvuq/base_element.py
+++ easyvvuq/base_element.py
@@ -39,18 +39,3 @@ class BaseElement(object):
 
     def element_category(self):
         raise NotImplementedError
-
-    def is_restartable(self):
-        return False
-
-    def get_restart_dict(self):
-        return None
-
-    def serialize(self):
-        return json.dumps({
-            "element_name": self.element_name(),
-            "element_version": self.element_version(),
-            "element_category": self.element_category(),
-            "restartable": self.is_restartable(),
-            "state": self.get_restart_dict()
-        })
