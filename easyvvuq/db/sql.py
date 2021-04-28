1080a9e623cb36362497c64beb0323471a316b07
Vytautas Jancauskas <unaudio@gmail.com>
2021-04-28 12:44:57 +0200

    unused import in sql.py

--- easyvvuq/db/sql.py
+++ easyvvuq/db/sql.py
@@ -10,7 +10,6 @@ from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import MetaData
-from sqlalchemy.exc import OperationalError
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import text
 from sqlalchemy.engine import Engine
