3e37184c229ee6e6f83751ef1ab6cfa25696f76f
Vytautas Jancauskas <unaudio@gmail.com>
2021-05-11 13:37:21 +0200

    added docstrings to simple_csv decoder

--- easyvvuq/decoders/simple_csv.py
+++ easyvvuq/decoders/simple_csv.py
@@ -1,3 +1,6 @@
+"""A Decoder for CSV format files.
+"""
+
 import os
 import logging
 import csv
@@ -31,37 +34,69 @@ logger = logging.Logger(__name__)
 
 
 class SimpleCSV(BaseDecoder, decoder_name="csv"):
+    """CSV Decoder.
 
+    Parameters
+    ----------
+    target_filename: str
+        Filename of a CSV file to decode.
+    ouput_columns: list
+        A list of column names that will be selected to appear in the output.
+    """
     def __init__(self, target_filename, output_columns):
-
         if len(output_columns) == 0:
             msg = "output_columns cannot be empty."
             logger.error(msg)
             raise RuntimeError(msg)
-
         self.target_filename = target_filename
         self.output_columns = output_columns
-
         self.output_type = OutputType('sample')
 
     @staticmethod
     def _get_output_path(run_info=None, outfile=None):
+        """Constructs absolute path from the `target_filename` and the `run_dir` parameter
+        in the `run_info` retrieved from the database.
+        
+        Parameters
+        ----------
+        run_info: dict
+            Run info as retrieved from the database.
+        outfile: str
+            Filename of the file to be parsed.
 
+        Returns
+        -------
+        str
+            An absolute path to the output file in the run directory.
+        """
         run_path = run_info['run_dir']
-
         if not os.path.isdir(run_path):
             raise RuntimeError(f"Run directory does not exist: {run_path}")
-
         return os.path.join(run_path, outfile)
 
     def parse_sim_output(self, run_info={}):
+        """Parses the CSV file and converts it to the EasyVVUQ internal dictionary based
+        format. The file is parsed in such a way that each column will appear as a vector
+        QoI in the output dictionary.
 
+        For example if the file contains the following data
+        a,b
+        1,2
+        3,4
+
+        And both `a` and `b` are specified as `output_columns` the output will look as follows
+        {'a': [1, 3], 'b': [2, 4]}.
+
+        Parameters
+        ----------
+        run_info: dict
+            Information about the run (used to retrieve construct the absolute path
+            to the CSV file that needs decoding.
+        """
         out_path = self._get_output_path(run_info, self.target_filename)
-
         results = {}
         for column in self.output_columns:
             results[column] = []
-
         with open(out_path, 'r', newline='') as csvfile:
             reader = csv.DictReader(csvfile)
             for row in reader:
@@ -72,5 +107,4 @@ class SimpleCSV(BaseDecoder, decoder_name="csv"):
                         results[column].append(row[column])
                     except KeyError:
                         raise RuntimeError('column not found in the csv file: {}'.format(column))
-
         return results
