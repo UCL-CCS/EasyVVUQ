841a5b955cdc6ed509ddb802ab81277fdd1142f0
Vytautas Jancauskas <unaudio@gmail.com>
2021-05-14 16:14:48 +0200

    monkey patch in test_actions_kubernetes.py

--- tests/test_actions_kubernetes.py
+++ tests/test_actions_kubernetes.py
@@ -1,6 +1,12 @@
 import pytest
 import os
+from unittest.mock import MagicMock
 from easyvvuq.actions.execute_kubernetes import ExecuteKubernetes
+import easyvvuq.actions.execute_kubernetes as execute_kubernetes
+
+execute_kubernetes.config = MagicMock()
+execute_kubernetes.V1ConfigMap = MagicMock()
+execute_kubernetes.V1ObjectMeta = MagicMock()
 
 
 def test_execute_kubernetes():
@@ -8,4 +14,5 @@ def test_execute_kubernetes():
         "orbitfold/easyvvuq:latest",
         "/EasyVVUQ/tutorials/sir /config/input.json && cat output.csv",
         output_file_name='output.csv')
+
 
