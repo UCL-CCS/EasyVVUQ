8576d50de12092cd19e3468a73c1cfbc9be06563
Bartosz Bosak <bbosak@man.poznan.pl>
2021-12-10 14:58:33 +0100

    enabled different flavours of argument passing

--- easyvvuq/actions/execute_qcgpj_task.py
+++ easyvvuq/actions/execute_qcgpj_task.py
@@ -11,29 +11,42 @@ if __name__ == "__main__":
 
     actions: Actions
 
-    if len(sys.argv) != 3:
+    print("Invoking execute_qcgpj_task with arguments: " + str(sys.argv))
+
+    if len(sys.argv) != 3 and len(sys.argv) != 5:
         sys.exit(
-            "Usage: python3 encoded_actions_object encoded_previous_object"
+            "Usage: python3 [-m module] encoded_actions_object encoded_previous_object"
         )
+
+    actions_f = None
+    previous_f = None
+
+    if len(sys.argv) == 3:
+        actions_f = sys.argv[1]
+        previous_f = sys.argv[2]
+
+    if len(sys.argv) == 5:
+        actions_f = sys.argv[3]
+        previous_f = sys.argv[4]
 
     if 'QCG_PM_EXEC_API_JOB_ID' not in os.environ:
         sys.exit("The required environment variable QCG_PM_STEP_ID not set")
 
     jobid = os.environ['QCG_PM_EXEC_API_JOB_ID']
 
-    with open(sys.argv[1]) as f:
+    with open(actions_f) as f:
         pickled_actions = f.read()
         pickled_actions_b64 = pickled_actions.encode('ascii')
         pickled_actions_b = base64.b64decode(pickled_actions_b64)
         actions = dill.loads(pickled_actions_b)
-    os.remove(sys.argv[1])
+    os.remove(actions_f)
 
-    with open(sys.argv[2]) as f:
+    with open(previous_f) as f:
         pickled_previous = f.read()
         pickled_previous_b64 = pickled_previous.encode('ascii')
         pickled_previous_b = base64.b64decode(pickled_previous_b64)
         previous = dill.loads(pickled_previous_b)
-    os.remove(sys.argv[2])
+    os.remove(previous_f)
 
     previous_next = actions.start(previous)
 
