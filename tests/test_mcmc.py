d00e4a825435f845d73f81d95a92c96dd56f9c84
David Coster <David.Coster@ipp.mpg.de>
2023-11-10 14:13:53 +0100

    reprted by code scanning

--- tests/test_mcmc.py
+++ tests/test_mcmc.py
@@ -3,7 +3,6 @@ import easyvvuq as uq
 import numpy as np
 import chaospy as cp
 import json
-import matplotlib.pyplot as plt
 import pytest
 import sys
 from easyvvuq.actions import ExecutePython, Actions
