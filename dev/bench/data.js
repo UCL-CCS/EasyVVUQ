1469d8a76732e20970bf36e8455f493fc6530bd4
github-action-benchmark <github@users.noreply.github.com>
2025-06-20 13:31:01 +0000

    add Benchmark (pytest) benchmark result for 3d7727de7ff106f2bdf27ef4166d5b5e7decc6a2

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1750423618219,
+  "lastUpdate": 1750426261407,
   "repoUrl": "https://github.com/UCL-CCS/EasyVVUQ",
   "entries": {
     "Benchmark": [
@@ -66,6 +66,72 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.00024977512375035164",
             "extra": "mean: 17.73194187036872 msec\nrounds: 54"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "maziarghorbani@gmail.com",
+            "name": "Maziar Ghorbani",
+            "username": "mzrghorbani"
+          },
+          "committer": {
+            "email": "maziarghorbani@gmail.com",
+            "name": "Maziar Ghorbani",
+            "username": "mzrghorbani"
+          },
+          "distinct": true,
+          "id": "3d7727de7ff106f2bdf27ef4166d5b5e7decc6a2",
+          "message": "Add conditional logic for Docker Hub authentication (build for all, push only with secret)",
+          "timestamp": "2025-06-20T14:26:10+01:00",
+          "tree_id": "db8ce02e3983a105501bbede97b38bac53a89356",
+          "url": "https://github.com/UCL-CCS/EasyVVUQ/commit/3d7727de7ff106f2bdf27ef4166d5b5e7decc6a2"
+        },
+        "date": 1750426260821,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/test_db_benchmark.py::test_draw",
+            "value": 0.11416312098672363,
+            "unit": "iter/sec",
+            "range": "stddev: 0.09099308537735086",
+            "extra": "mean: 8.759396128599997 sec\nrounds: 5"
+          },
+          {
+            "name": "tests/test_db_benchmark.py::test_store_results",
+            "value": 0.20170751945703008,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0707002632168006",
+            "extra": "mean: 4.957673381200005 sec\nrounds: 5"
+          },
+          {
+            "name": "tests/test_db_benchmark.py::test_get_collation_result",
+            "value": 109.72220971067127,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00031508045633757496",
+            "extra": "mean: 9.113925089887639 msec\nrounds: 89"
+          },
+          {
+            "name": "tests/test_db_benchmark.py::test_draw_add",
+            "value": 0.1153866020445603,
+            "unit": "iter/sec",
+            "range": "stddev: 0.11815399604297513",
+            "extra": "mean: 8.666517448999992 sec\nrounds: 5"
+          },
+          {
+            "name": "tests/test_db_benchmark.py::test_store_results_add",
+            "value": 0.20606748246555626,
+            "unit": "iter/sec",
+            "range": "stddev: 0.027070329633180495",
+            "extra": "mean: 4.852779235399976 sec\nrounds: 5"
+          },
+          {
+            "name": "tests/test_db_benchmark.py::test_get_collation_result_add",
+            "value": 53.409526706779424,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00022818791544108223",
+            "extra": "mean: 18.72325148077126 msec\nrounds: 52"
           }
         ]
       }
